babbad525929b26df25b3562dfdb9d6da1e36f28
xxi-noxx <handlkou@yahoo.co.jp>
2015-10-21 20:10:35 +0900

    add new file

---
+++ UenoStudyEXVS/Calc/Calc.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Calc
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.plusResult = new System.Windows.Forms.Label();
+            this.plus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // plusResult
+            // 
+            this.plusResult.AutoSize = true;
+            this.plusResult.Location = new System.Drawing.Point(201, 156);
+            this.plusResult.Name = "plusResult";
+            this.plusResult.Size = new System.Drawing.Size(52, 18);
+            this.plusResult.TabIndex = 2;
+            this.plusResult.Text = "label1";
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(57, 150);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(100, 30);
+            this.plus.TabIndex = 2;
+            this.plus.Text = "足し算";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 403);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.plusResult);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Calc";
+            this.Text = "電卓";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label plusResult;
+        private System.Windows.Forms.Button plus;
+    }
+}
+
---
+++ UenoStudyEXVS/Calc/Calc.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private enum CalcType : short
+        {
+            plus,
+            minus,
+            multiplication,
+            division
+        }
+        
+        private void Calc_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                InitializeForm();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "アプリケーションエラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if(btn.Name == this.plus.Name)
+                {
+                    this.plusResult.Text = Run(this.textBox1.Text, this.textBox2.Text, CalcType.plus).ToString("#,##0.###");
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                MessageBox.Show(ex.Message, "変換エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "アプリケーションエラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InitializeForm()
+        {
+            this.textBox1.Text = string.Empty;
+            this.textBox2.Text = string.Empty;
+        }
+        
+        private double Run(string val1, string val2, CalcType type)
+        {
+            double dVal1, dVal2, result = 0;
+            if (!double.TryParse(val1, out dVal1))
+            {
+                throw new InvalidCastException("引数１を数値に変換出来ません。");
+            }
+            if (!double.TryParse(val2, out dVal2))
+            {
+                throw new InvalidCastException("引数２を数値に変換出来ません。");
+            }
+
+            switch (type)
+            {
+                case CalcType.plus:
+                    result = dVal1 + dVal2;
+                    break;
+            }
+            return result;
+        }
+
+        private bool LeapYearCheck(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
